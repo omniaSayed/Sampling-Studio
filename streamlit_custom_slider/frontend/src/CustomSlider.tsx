80265068052d299f3e0edc2cb100dd47a0a98bcb
SamaMostafa1 <93403723+SamaMostafa1@users.noreply.github.com>
2022-10-18 21:08:42 +0200

    Add files via upload
    Sama

--- streamlit_custom_slider/frontend/src/CustomSlider.tsx
+++ streamlit_custom_slider/frontend/src/CustomSlider.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react"
 import { ComponentProps, Streamlit, withStreamlitConnection } from "streamlit-component-lib"
 import { Slider } from "baseui/slider"
+import { SERVFAIL } from "dns"
 
 /**
  * We can use a Typescript interface to destructure the arguments from Python
  * and validate the types of the input
  */
 interface PythonArgs {
-  label: string
+  label : string
   minValue?: number
   maxValue?: number
   initialValue: number[]
@@ -23,17 +24,16 @@ const CustomSlider = (props: ComponentProps) => {
   // This ensures typing validation for received props from Python
   const { label, minValue, maxValue, initialValue }: PythonArgs = props.args
   const [value, setValue] = useState(initialValue)
-
-  useEffect(() => Streamlit.setFrameHeight())
+  useEffect(() => Streamlit.setFrameHeight(130))
 
   return (
     <>
-      <h3>{label}</h3>
-      <Slider
+      <h3 style={{backgroundColor: " rgba(1, 10, 26, 0)", textAlign:"left", fontSize:20, color:"white",borderBottomColor:"#042a4262"}}>{label} </h3>
+      <Slider     
         value={value}
         onChange={({ value }) => value && setValue(value)}
         onFinalChange={({ value }) => Streamlit.setComponentValue(value)}
-        min={minValue}
+        min={minValue} 
         max={maxValue}
       />
     </>

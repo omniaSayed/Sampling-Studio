12a9b4546329030485d323cb5db2fd624b9ac1af
SamaMostafa1 <93403723+SamaMostafa1@users.noreply.github.com>
2022-10-21 12:41:37 +0200

    Add files via upload
    sama

--- app.py
+++ app.py
@@ -27,25 +27,17 @@ st.title("Sampling Studio For Biological Signals")
 st.markdown(" Welcome To Our Sampling Studio ")
 st.sidebar.title("Sampling Settings")
 #Add elements to side bar
-with st.sidebar:
-    #slider to get signal to noise ratio
-    SNR= st.slider('SNR', 0, 20,0,key='SNR')
-    #slider to get frequency for sin wave generation
-    frequency = st.slider('Frequency', 0.0, 20.0, step=0.5, key='Frequency')
-    #slider to get amplitude for sin wave generation
-    amplitude = st.slider('Amplitude', 0, 20, 0, key='Amplitude')
-    #slider to get phase for sin wave generation
-    phase = st.slider('Phase', 0, 20, 0, key='Phase')
     #select box used to determine type pf provided signals
-    selected_signal = st.selectbox('Provided Signals', ['Generate A Random Signal', 'EKG Sample Signal', 'ECG Sample Signal', 'EMG Sample Signal', 'EEG Sample Signal', 'Generate sine ', 'remove','Provide A Local File Signal'])
+selected_signal = st.sidebar.selectbox('Provided Signals', ['Generate A Random Signal', 'EKG Sample Signal', 'ECG Sample Signal', 'EMG Sample Signal', 'EEG Sample Signal', 'Generate sine ', 'remove','Provide A Local File Signal'])
     #slider to provide maximum frequency of signal for sampling process
-    def set_slider(max_freq):
+def set_slider(max_freq):
         st.slider('Change Sampling Maximum Frequency ', 1,3*max_freq )
 col1, col2 = st.columns(2)
 ################################## Adding variables to session ######################################################
 if 'list_of_signals' not in st.session_state:
     st.session_state['list_of_signals']=[]
     st.session_state['sum_of_signals']=np.zeros(1000)
+    st.session_state['sum_of_signals_clean']=np.zeros(1000)
     st.session_state['fig_sine']=go.Figure()
 ################################## global variables  ######################################################
 #cash using(mini memory for the front end)
@@ -111,21 +103,16 @@ def initialize_plot(fig):
         t=100,
         pad=4
     ),
-    paper_bgcolor="rgba(2, 2, 46, 0.925)",
-    font_color="white",
+    paper_bgcolor="white",
+    font_color="black",
 )
     st.plotly_chart(fig,use_container_width=True)
 #function used to add signal to same plot
 def add_to_plot(fig,x,y,name):
-     if SNR==0 :
-         fig.add_trace(
-        go.Scatter(x=x, y=y, name=name)
-    )   
-     else:
-        noised_signal=createNoise(SNR,y)
         fig.add_trace(
         go.Scatter(x=x, y=y, name=name)
-    )
+    )   
+    
 #function used to plot the summation of sin signal generated
 def plot(x,y):
     
@@ -143,8 +130,8 @@ def plot(x,y):
         t=100,
         pad=4
     ),
-    paper_bgcolor="rgba(2, 2, 46, 0.925)",
-    font_color="white",
+    paper_bgcolor="white",
+    font_color="black",
 )
     st.plotly_chart(fig,use_container_width=True)
 #function used to delete signals from the plot
@@ -161,6 +148,38 @@ def delete(index_to_delete):
     #remove the signal from the summation 
     removed_signal = amplitude * sin(2 * pi * freq* time + phase)
     st.session_state.sum_of_signals-=removed_signal
+def noise_sine(index_signal):
+    #get parameter to edit add noise
+    # freq= st.session_state.list_of_signals[index_signal][0]
+    # amplitude=st.session_state.list_of_signals[index_signal][1]
+    # phase=st.session_state.list_of_signals[index_signal ][2]
+    if st.session_state.noise_key==0:
+    # #     signal_new=amplitude * sin(2 * pi * freq* time + phase)
+    # #     signal_parameters=[freq,amplitude,phase]
+    # #     list_fig_sine_data=list(st.session_state.fig_sine.data)
+    # #     list_fig_sine_data.remove(st.session_state.fig_sine.data[index_signal])
+    # #     st.session_state.fig_sine.data=tuple(list_fig_sine_data)
+    # #     add_to_plot(st.session_state.fig_sine,x=time,y=signal_new,name=f"sin{freq}hz")
+    # #     st.session_state.list_of_signals.append(signal_parameters)
+       st.session_state.sum_of_signals=st.session_state.sum_of_signals_clean
+    else:
+        #remove signal without noise
+        # st.session_state.list_of_signals.pop(index_signal)
+        # list_fig_sine_data=list(st.session_state.fig_sine.data)
+        # list_fig_sine_data.remove(st.session_state.fig_sine.data[index_signal])
+        # st.session_state.fig_sine.data=tuple(list_fig_sine_data)
+        # #Create signal with noise 
+        # removed_signal = amplitude * sin(2 * pi * freq* time + phase)
+        # st.session_state.sum_of_signals-=removed_signal
+        #signal_new=amplitude * sin(2 * pi * freq* time + phase)
+        noised_sine_sig=createNoise(st.session_state.noise_key,st.session_state.sum_of_signals_clean ) 
+        #signal_parameters=[freq,amplitude,phase]
+        #add_to_plot(st.session_state.fig_sine,x=time,y=noised_sine_sig,name=f"sin{freq}hz")
+        #st.session_state.list_of_signals.append(signal_parameters)
+        st.session_state.sum_of_signals=noised_sine_sig
+
+        
+    
 ################################## Main implementation ###################################################### 
 if selected_signal == "Generate A Random Signal":
     Fs=40
@@ -170,22 +189,26 @@ if selected_signal == "Generate A Random Signal":
     t=np.linspace(0,Tw,num=N)
     random_signal = generate_signal(t)
     fig = px.line(x=t, y=random_signal)
-    st.plotly_chart(fig)
+    st.plotly_chart(fig,use_container_width=True)
 
 elif selected_signal == "EMG Sample Signal":
+    #slider to get signal to noise ratio
+    SNR= st.sidebar.slider('SNR', 0, 20,0,key='SNR')
     maximum_frequency=500
     set_slider(maximum_frequency)
     emg = load_data( selected_signal )
     if SNR==0 :
         fig = px.line(x=emg.t, y=emg.emg)
-        st.plotly_chart(fig)
+        st.plotly_chart(fig,use_container_width=True)
     else:
         emg_m=np.array(emg.emg)
         noised_signal=createNoise(SNR,emg_m)
         noise_fig=px.line(noised_signal)
-        st.plotly_chart(noise_fig)
+        st.plotly_chart(noise_fig,use_container_width=True)
 
 elif selected_signal == "EKG Sample Signal":
+    #slider to get signal to noise ratio
+    SNR= st.sidebar.slider('SNR', 0, 20,0,key='SNR')
     maximum_frequency=500
     set_slider(maximum_frequency)
     emg = load_data( selected_signal )
@@ -193,6 +216,8 @@ elif selected_signal == "EKG Sample Signal":
     st.plotly_chart(fig,use_container_width=True)
 
 elif selected_signal == "ECG Sample Signal":
+    #slider to get signal to noise ratio
+    SNR= st.sidebar.slider('SNR', 0, 20,0,key='SNR')
     ecg = load_data( selected_signal ) 
     if SNR==0 :
         fig = px.line(ecg[0:300])
@@ -204,9 +229,11 @@ elif selected_signal == "ECG Sample Signal":
         st.plotly_chart(noise_fig,use_container_width=True)
 
 elif selected_signal == "EEG Sample Signal":
+    #slider to get signal to noise ratio
+    SNR= st.sidebar.slider('SNR', 0, 20,0,key='SNR')
     maximum_frequency=500
     set_slider(maximum_frequency)
-    eeg = load_data( selected_signal )
+    eeg = load_data( selected_signal,use_container_width=True )
     if SNR==0 :
         fig = px.line(x=eeg.t, y=eeg.eeg)
         st.plotly_chart(fig)
@@ -217,8 +244,16 @@ elif selected_signal == "EEG Sample Signal":
         st.plotly_chart(noise_fig,use_container_width=True)
     
 elif selected_signal == 'Generate sine ':
+    with st.sidebar:
+        #slider to get frequency for sin wave generation
+        frequency = st.slider('Frequency', 0.0, 20.0, step=0.5, key='Frequency')
+        #slider to get amplitude for sin wave generation
+        amplitude = st.slider('Amplitude', 0, 20, 0, key='Amplitude')
+        #slider to get phase for sin wave generation
+        phase = st.slider('Phase', 0, 20, 0, key='Phase')
     time = np.linspace(0, 5, 1000)
-    genrate_button=st.button('genrate',key=0)
+    with col1:
+      genrate_button=st.sidebar.button('genrate',key=0)
     if genrate_button:
         signal_parameters=[frequency,amplitude,phase]
         sine_volt = amplitude * sin(2 * pi * frequency * time + phase)
@@ -228,16 +263,24 @@ elif selected_signal == 'Generate sine ':
         
         st.session_state.list_of_signals.append(signal_parameters)
         st.session_state.sum_of_signals+=sine_volt
+        st.session_state.sum_of_signals_clean=st.session_state.sum_of_signals
     if st.session_state.list_of_signals:
-        option = st.selectbox(
+        option = st.sidebar.selectbox(
         'Select Values to Delete',
         st.session_state.list_of_signals,)
         selected_value=st.session_state.list_of_signals.index(option)
-        delete_button=st.button('delete',key=1,on_click=delete,args= (selected_value,))
+        with col2:
+           delete_button=st.sidebar.button('delete',key=1,on_click=delete,args= (selected_value,))
     with col1:
           initialize_plot(st.session_state.fig_sine)
     with col2:
-        plot(x=time,y=st.session_state.sum_of_signals)   
+        plot(x=time,y=st.session_state.sum_of_signals)
+    if st.session_state.list_of_signals:
+        # signal_noise= st.sidebar.selectbox(
+        #     'Select Values to be noised',
+        #     st.session_state.list_of_signals)
+        # selected_signal=st.session_state.list_of_signals.index(option)
+        noise_sin=st.slider('SNR sine',key="noise_key",on_change=noise_sine,args= (selected_signal,))  
 elif selected_signal == 'Provide A Local File Signal':
     uploaded_file = st.file_uploader("Please choose a CSV or TXT file", accept_multiple_files=False,type=['csv','txt'])
     maximum_frequency=500
